32771393b9b72d0ef221d86b07a17b66be08d9a2
DaviM <petmania20072008@gmail.com>
2025-06-03 21:18:09 -0300

    Atualizarcasadavi

--- app/routes/router.js
+++ app/routes/router.js
@@ -7,6 +7,71 @@ router.get("/",
     res.render("pages/index");    
 });
 
+router.get("/Cadastroadestrador.ejs", 
+    function (req, res) {
+    res.render("pages/Cadastroadestrador.ejs");    
+});
+
+// Nova rota para cadastro de cliente (caso precise)
+router.get("/cliente.ejs", 
+    function (req, res) {
+    res.render("pages/cliente.ejs");    
+});
+
+router.get("/agendamentoadestrador.ejs", 
+    function (req, res) {
+    res.render("pages/agendamentoadestrador.ejs");    
+});
+
+router.get("/clienteadestrador.ejs", 
+    function (req, res) {
+    res.render("pages/clienteadestrador.ejs");    
+});
+
+router.get("/Login.ejs", 
+    function (req, res) {
+    res.render("pages/Login.ejs");    
+});
+
+router.get("/mensagensadestrador.ejs", 
+    function (req, res) {
+    res.render("pages/mensagensadestrador.ejs");    
+});
+
+router.get("/paineladestrador.ejs", 
+    function (req, res) {
+    res.render("pages/paineladestrador.ejs");    
+});
+
+router.get("/perfiladestrador.ejs", 
+    function (req, res) {
+    res.render("pages/perfiladestrador.ejs");    
+});
+
+router.get("/planosadestrador.ejs", 
+    function (req, res) {
+    res.render("pages/planosadestrador.ejs");    
+});
+
+router.get("/tipodeusuario.ejs", 
+    function (req, res) {
+    res.render("pages/tipodeusuario.ejs");    
+});
+
+router.get("/clientesadestrador.ejs", 
+    function (req, res) {
+    res.render("pages/clientesadestrador.ejs");    
+});
+
+
+
+
+
+
+
+
+
+
 router.post("/exibir", 
     function (req, res) {
 
